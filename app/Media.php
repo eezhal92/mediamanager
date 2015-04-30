70f201e0ce051c795ba2c9bf76ce43eacfe14f1b
eezhal92 <arkinthesky.69@gmail.com>
2015-04-30 10:16:32 +0700

    add variable

--- app/Media.php
+++ app/Media.php
@@ -3,11 +3,25 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model {
+	protected $table = 'medias';
 
 	protected $fillable = ['filename', 'path', 'mime_type', 'size'];
 
 	public $timestamps = true;
 
     public $appends = ['extension', 'formatted_size', 'file_path'];
+
+    // variable for extension
+
+    public static $images = ['jpg', 'jpeg', 'png']; 
+
+    public static $documents = ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf'];
+
+    public static $archives = ['rar', 'zip'];
+
+    // Accessor
+    
+
+    // Utility Function 
 
 }
